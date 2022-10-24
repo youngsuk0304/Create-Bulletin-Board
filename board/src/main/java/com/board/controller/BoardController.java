36eb4c91bab6882f0782639e2729e42aedc00dbf
youngsuk <33721972+youngsuk0304@users.noreply.github.com>
2022-10-24 15:36:51 +0900

    조금씩 변경

--- board/src/main/java/com/board/controller/BoardController.java
+++ board/src/main/java/com/board/controller/BoardController.java
@@ -1,51 +1,16 @@
 package com.board.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
-/**
- * Handles requests for the application home page.
- */
-
 @Controller
-public class BoardController{
-	@Autowired
-	BoardDao boardDao;
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		List<BoardDto> list = boardDao.sel();
-		for(int i=0; i<list.size(); i++){
-			model.addAttribute("board_no", list.get(i).getBoard_no());
-			model.addAttribute("board_title", list.get(i).getBoard_title() );
-			model.addAttribute("board_content", list.get(i).getBoard_content() );
-			model.addAttribute("board_writer", list.get(i).getBoard_writer() );
-		}
-		return "home";
-	}
-	
+@RequestMapping("/board/*")
+public class BoardController {
+
+ @RequestMapping(value = "/list", method = RequestMethod.GET)
+ public void getList() throws Exception {
+  
+   
+ }
 }
--- board/src/main/webapp/WEB-INF/views/home.jsp
+++ board/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,7 +1,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
-<%@ page session="false" %>
 <html>
 <head>
 	<title>Home</title>
@@ -12,5 +12,11 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+<P> board_no is ${board_no}</P>
+<P> board_title is ${board_title}</P>
+<P> board_content is ${board_content}</P>
+<P> board_writer is ${board_writer}</P>
+<p><a href="/board/list">게시물 목록</a></p>
+
 </body>
 </html>
