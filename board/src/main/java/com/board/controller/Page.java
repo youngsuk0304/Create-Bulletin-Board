45872c155c1a6c123bb3406e4350b778958dcf70
youngsuk <33721972+youngsuk0304@users.noreply.github.com>
2022-11-03 11:52:23 +0900

    20221103 서영석 Update Page

--- board/src/main/java/com/board/controller/Page.java
+++ board/src/main/java/com/board/controller/Page.java
@@ -115,4 +115,44 @@ public class Page {
 		next = endPageNum * displayPost >= count ? false : true;
 		 
 	}
+//	private String searchTypeKeyword; 
+//
+//	public void setSearchTypeKeyword(String searchType, String keyword) {
+//	 
+//		if(searchType.equals("") || keyword.equals("")) {
+//			searchTypeKeyword = ""; 
+//		} else {
+//			searchTypeKeyword = "&searchType=" + searchType + "&keyword=" + keyword; 
+//		}  
+//	}
+
+	public String getSearchTypeKeyword() {
+		 
+		if(searchType.equals("") || keyword.equals("")) {
+			return ""; 
+		} else {
+			return "&searchType=" + searchType + "&keyword=" + keyword; 
+		}
+	}
+
+	private String searchType;
+	private String keyword; 
+
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;  
+	}
+
+	public String getSearchType() {
+		return searchType;
+	} 
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;  
+	} 
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	
 }
